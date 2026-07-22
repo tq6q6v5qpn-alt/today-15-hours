16a82e8c5629a65e85cb8bde0bdb631aca596798
noblestar <tq6q6v5qpn@privaterelay.appleid.com>
2026-07-22 16:38:00 +0900

    Add portable backups and family testing tools

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='today15-v4';
+const CACHE='today15-v5';
 const FILES=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
