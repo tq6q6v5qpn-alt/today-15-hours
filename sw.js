d8a2f4f8637cb108dbdba7bf91569e3d834dbe43
noblestar <tq6q6v5qpn@privaterelay.appleid.com>
2026-07-24 12:31:02 +0900

    Enlarge preparation and travel controls

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='today15-v12';
-const FILES=['./','./index.html','./styles.css?v=12','./core.js?v=12','./app.js?v=12','./manifest.webmanifest'];
+const CACHE='today15-v13';
+const FILES=['./','./index.html','./styles.css?v=13','./core.js?v=13','./app.js?v=13','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index.html')))));
